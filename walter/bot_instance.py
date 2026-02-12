5df62082c8cd98e00a676e3fce7d264e1f4def8d
efar301 <evan.farns301@gmail.com>
2026-02-12 22:09:31 +0000

    update

--- walter/bot_instance.py
+++ walter/bot_instance.py
@@ -4,5 +4,5 @@ from discord.ext import commands
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="\\", intents=intents, help_command=None)
+bot = commands.Bot(command_prefix="/", intents=intents, help_command=None)
 
