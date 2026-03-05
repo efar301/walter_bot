86e6d2fb02faead6c78c0b9f7f821a48c2bf71d6
farns <evanfarnsworth@george.local>
2026-03-04 19:13:15 -0800

    quickcommit

--- walter/sheet_functions.py
+++ walter/sheet_functions.py
@@ -74,15 +74,13 @@ def write_question(sheet_name, question):
 
     curr_date = datetime.now().date()
     col_list = sheet.batch_get(["B2:J5"], major_dimension="COLUMNS")[0]
-
     for i in range(len(col_list)):
         col_date = parse_us_date(col_list[i][0])
         if col_date > curr_date:
             
             # add 2 because cells start at B
             # row updated in sheet is 5
-            if len(col_list[i]) == 2: 
-                print(f"len == 2")
+            if len(col_list[i]) == 3: 
                 sheet.update_cell(5, i + 2, question)
             else:
                 curr_questions = col_list[i][3]
